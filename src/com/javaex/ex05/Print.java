04aa8822ebdf0573f317b09924f5cf7ec370015c
Mocking_jayk0918 <jesper2able@gmail.com>
2022-04-23 18:02:36 +0900

    1.0(java_practice 22.04.23 18:02)

--- src/com/javaex/ex05/Print.java
+++ src/com/javaex/ex05/Print.java
@@ -2,10 +2,34 @@ package com.javaex.ex05;
 
 public class Print {
     
+	// 필드 
+
+	// 생성자
+	
+	// 메소드 gs
+	
+	// 메소드 일반
+	
     public void printer(int val){
         System.out.println(val);
     }
-
+    
+    public void printer(boolean action) {
+    	if(action == true) {
+    		System.out.println("true");
+    	}else {
+    		System.out.println("false");
+    	}
+    }
+    
+    public void printer(double val) {
+    	System.out.println(val);
+    }
+    
+    public void printer(String word) {
+    	System.out.println(word);
+    }
+    
     //메소드  3개 를 작성하세요
     
 }
