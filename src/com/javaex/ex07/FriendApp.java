22a5688e901d6de50b3bad468a647402740829e7
Mocking_jayk0918 <jesper2able@gmail.com>
2022-04-25 00:00:01 +0900

    1.0(java_practice 22.04.24 23:59)

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -13,15 +13,19 @@ public class FriendApp {
         System.out.println("친구를 3명 등록해 주세요");
         
         for (int i = 0; i < friendArray.length; i++) {
+        	friendArray[i] = new Friend();
         	System.out.print("이름: ");
-        	friendArray[i].name = sc.nextLine();
+        	friendArray[i].name(sc.nextLine());
         	System.out.print("핸드폰: ");
-        	friendArray[i].hp = sc.nextLine();
+        	friendArray[i].hp(sc.nextLine());
         	System.out.print("학교: ");
-        	friendArray[i].school = sc.nextLine();
+        	friendArray[i].school(sc.nextLine());
         	System.out.println("-------------------------");
         }
         
+        for (int i = 0; i < friendArray.length; i++) {
+        	friendArray[i].showInfo();
+        }
         
         sc.close();
     }
