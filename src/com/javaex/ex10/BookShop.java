5f89104e3fe89337fd5e544e9eba5169b2822456
Mocking_jayk0918 <jesper2able@gmail.com>
2022-04-25 19:06:37 +0900

    1.0(java_practice 22.04.25 19:06)

--- src/com/javaex/ex10/BookShop.java
+++ src/com/javaex/ex10/BookShop.java
@@ -24,13 +24,11 @@ public class BookShop {
         Scanner scanner = new Scanner(System.in);
         System.out.print("대여 하고 싶은 책의 번호를 입력하세요:");
         int num = scanner.nextInt();
-       
 
         // (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
         // 코드작성
+        books[num-1].rent();
         
-        
-
         System.out.println("*****도서 정보 출력하기******");
         displayBookInfo(books);
         
@@ -39,7 +37,9 @@ public class BookShop {
 
     //(2)전달받은 배열을 모두 출력하는 메소드
     private static void displayBookInfo(Book[] books) {
-        
-    	//코드작성
+    	for(int i=0; i<books.length; i++) {
+    		books[i].print();
+    	}
+    	
     }
 }
